1082512eaff298cb79546198f9a4395a0a484a8f
AshleyAbramowiczGibbs <ashleyabramo2@gmail.com>
2018-11-25 16:25:59 -0600

    Start Front End for shopdream

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,66 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+
+      <button v-on:click="createStyle()" class="btn">Upload a new style</button>
+      Image: <input v-model="newStyleImage" type="text">
+
+      <div v-for="style in styles">
+        <img v-bind:src="style.image_url">
+        <p>url: {{ style.image_url }}</p>
+        
+      </div>
+      
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+</style>
 
+<script>
+let axios = require("axios");
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      message: "Welcome to Shop.Dream!",
+      styles: [],
+      newStyle: {},
+      newStyleImage: ""
+
+    };
+  },
+  created: function() {
+    axios.get("http://localhost:3000/api/styles").then(
+      function(response) {
+        console.log(response.data);
+        this.styles = response.data;
+      }.bind(this)
+    );
+  },
+  methods: {
+    createStyle: function() {
+      let params = {
+        image_url: this.newStyleImage,
+        user_id: 1
+      };
+      axios
+        .post("http://localhost:3000/api/styles", params)
+        .then(
+          function(response) {
+            console.log(response);
+            this.styles.push(response.data);
+            this.newStyleImage = "";
+          }.bind(this)
+        )
+        .catch(
+          function(error) {
+            console.log(error.response.data.errors);
+            this.errors = error.response.data.errors;
+          }.bind(this)
+        );
+    }
+  },
+
+  computed: {}
+};
 </script>
