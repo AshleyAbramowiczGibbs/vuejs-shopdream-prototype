8fd620485084b92ce02a8e54ca1f7ecc3fe60f52
AshleyAbramowiczGibbs <ashleyabramo2@gmail.com>
2018-12-12 00:03:06 -0600

    Navbar and padding

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link>
+      <router-link to="/"></router-link>
       <div>
         <nav>
           <div class="nav-wrapper">
-            <a href="#!" class="brand-logo"> shop.Dream </a>
+            <a href="#/" class="brand-logo"> shop.Dream </a>
             <ul class="right hide-on-med-and-down">
               <li>
                 <a href="sass.html"><i class="material-icons">favorite_border</i></a>
               </li>
               <li>
-                <a href="badges.html"><i class="material-icons">person_outline</i></a>
+                <a href="/#/login"><i class="material-icons">person_outline</i></a>
               </li>
               <li>
                 <a href="collapsible.html"><i class="material-icons">refresh</i></a>
@@ -37,14 +37,25 @@ body {
 
 nav {
   background-color: #ffff;
+  height: 100px;
 }
 
 a.brand-logo {
   color: #212121;
   font-family: Pacifico;
+  padding: 10px 0px 20px 50px;
 }
 
 i.material-icons {
   color: #212121;
+  padding: 10px 0px 20px 20px;
+}
+
+div.home {
+  padding: 0% 20% 0% 20%;
+}
+
+label {
+  padding: 0px 0px 0px 10px;
 }
 </style>
